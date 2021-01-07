f9301b67d8b83789de896965582c8df377b8075d
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-01-07 13:28:41 +0000

    Create dependency_install.sh

---
+++ dependency_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x 
+apt install libncursesw5-dev
+apt install libelf-dev
+apt install systemtap-sdt-dev
+apt install libslang2-dev
+apt install libgtk2.0-dev
+apt install libperl-dev
+apt install python-dev
+apt install liblzma-dev
+apt install libzstd-dev
+apt install libcap-dev
+apt install libnuma-dev
+apt install libbabeltrace-ctf-dev
+apt install libdw-dev
+apt install binutils-dev
+apt install libunwind-dev
+apt install libaudit-dev
