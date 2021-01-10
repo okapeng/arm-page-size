e868ac4cfe359635564625fcf24519fd2c947aff
root <root@ms0313.utah.cloudlab.us>
2021-01-09 23:36:20 -0700

    add run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x 
+
+./triple_run.sh ./microbenchmark sequential 10
+./triple_run.sh ./time_microbenchmark sequential 10
+./triple_run.sh ./microbenchmark random 10
+./triple_run.sh ./time_microbenchmark random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
