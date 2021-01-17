784479b0d00d5817d2529a5460c537596fb2f968
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-01-17 21:56:57 +0000

    Update dependency_install.sh

--- dependency_install.sh
+++ dependency_install.sh
@@ -4,3 +4,4 @@ apt-get install apt apt-utils dpkg dpkg-dev module-init-tools pciutils open-iscs
 apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev libncursesw5-dev
 apt-get install libelf-dev systemtap-sdt-dev libslang2-dev libgtk2.0-dev libperl-dev python-dev
 apt-get install liblzma-dev libzstd-dev libcap-dev libnuma-dev libbabeltrace-ctf-dev libdw-dev binutils-dev libunwind-dev libaudit-dev libiberty-dev
+echo "Please commenting out first two lines in file /lib/modules-load.d/open-iscsi.conf"
