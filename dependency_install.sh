e0bd437e9b24b53326d7f070c8a4a7bf43d0bf37
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-01-17 21:35:49 +0000

    Update dependency_install.sh

--- dependency_install.sh
+++ dependency_install.sh
@@ -1,19 +1,6 @@
 #!/bin/bash -x 
-apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev
-apt-get install libncurses5-dev libncursesw5-dev
-apt-get install libelf-dev
-apt-get install systemtap-sdt-dev
-apt-get install libslang2-dev
-apt-get install libgtk2.0-dev
-apt-get install libperl-dev
-apt-get install python-dev
-apt-get install liblzma-dev
-apt-get install libzstd-dev
-apt-get install libcap-dev
-apt-get install libnuma-dev
-apt-get install libbabeltrace-ctf-dev
-apt-get install libdw-dev
-apt-get install binutils-dev
-apt-get install libunwind-dev
-apt-get install libaudit-dev
-apt-get install libiberty-dev
+apt update
+apt-get install apt apt-utils dpkg dpkg-dev module-init-tools pciutils open-iscsi
+apt-get install build-essential linux-source bc kmod cpio flex libncurses5-dev libelf-dev libssl-dev libncursesw5-dev
+apt-get install libelf-dev systemtap-sdt-dev libslang2-dev libgtk2.0-dev libperl-dev python-dev
+apt-get install liblzma-dev libzstd-dev libcap-dev libnuma-dev libbabeltrace-ctf-dev libdw-dev binutils-dev libunwind-dev libaudit-dev libiberty-dev
