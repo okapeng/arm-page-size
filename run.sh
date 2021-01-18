7dd36331d65e98975d8fda58f6a4505e90e2243d
root <root@ms0313.utah.cloudlab.us>
2021-01-18 16:10:07 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,17 @@
 ./triple_run.sh ./microbenchmark random 10
 ./triple_run.sh ./time_microbenchmark random 10
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+sleep 1
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+sleep 1
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+sleep 1
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark sequential 10
+sleep 1
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+sleep 1
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+sleep 1
 ./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+sleep 1
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random 10
