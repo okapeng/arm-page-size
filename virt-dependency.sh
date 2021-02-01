368554e352401c79dfcee4c914ec4cfda6f5a5fc
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-01 16:52:16 +0000

    Update virt-dependency.sh

--- virt-dependency.sh
+++ virt-dependency.sh
@@ -1,2 +1,2 @@
-apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager cpu-checker qemu-efi
+apt install -y qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager cpu-checker qemu-efi libmount-dev cmake
 systemctl status libvirtd
