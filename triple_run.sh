92614bbab9d22be06d202443954f81eba1fd6744
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-16 18:00:01 +0000

    Update triple_run.sh

--- triple_run.sh
+++ triple_run.sh
@@ -11,16 +11,6 @@ ACCESS_PATTERN=$2 # specify the access pattern, random or sequential
 MEMORY=$3 # specify memory size for each process in GB
 NUM_RUNS=20 # number of runs
 
-# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_1=$!
-# wait $pid_1
-# echo "process 1 terminates"
-# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_2=$!
-# wait $pid_2
-# echo "process 2 terminates"
-# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_3=$!
-# wait $pid_3
-# echo "process 3 terminates"
-
 for i in {1..$NUM_RUNS}
 do
 	$BENCHMARK $ACCESS_PATTERN $MEMORY & pid=$!
