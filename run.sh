8fad79679d192428bf0be5c32932e9d668f1b7be
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-16 17:47:13 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,29 @@
 #!/bin/bash -x 
 
-./triple_run.sh ./microbenchmark sequential 10
-./triple_run.sh ./time_microbenchmark sequential 10
-./triple_run.sh ./microbenchmark random 10
-./triple_run.sh ./time_microbenchmark random 10
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+if [ -z "$1" ]
+	then
+	echo "No argument supplied"
+	exit 
+fi
+
+MEMORY=$1
+
+./triple_run.sh ./microbenchmark sequential $MEMORY
+./triple_run.sh ./time_microbenchmark sequential $MEMORY
+./triple_run.sh ./microbenchmark random $MEMORY
+./triple_run.sh ./time_microbenchmark random $MEMORY
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential $MEMORY
 sleep 1
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential $MEMORY
 sleep 1
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual sequential $MEMORY
 sleep 1
-./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark sequential 10
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark sequential $MEMORY
 sleep 1
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random $MEMORY
 sleep 1
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random $MEMORY
 sleep 1
-./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random $MEMORY
 sleep 1
-./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random 10
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random $MEMORY
