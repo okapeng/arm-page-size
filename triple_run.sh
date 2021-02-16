512b958fa30845ad418eaf16f089f932b560a52a
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-16 17:54:09 +0000

    Update triple_run.sh

--- triple_run.sh
+++ triple_run.sh
@@ -9,13 +9,21 @@ fi
 BENCHMARK=$1 # specify the benchmark to run
 ACCESS_PATTERN=$2 # specify the access pattern, random or sequential
 MEMORY=$3 # specify memory size for each process in GB
+NUM_RUNS=20 # number of runs
 
-$BENCHMARK $ACCESS_PATTERN $MEMORY & pid_1=$!
-wait $pid_1
-echo "process 1 terminates"
-$BENCHMARK $ACCESS_PATTERN $MEMORY & pid_2=$!
-wait $pid_2
-echo "process 2 terminates"
-$BENCHMARK $ACCESS_PATTERN $MEMORY & pid_3=$!
-wait $pid_3
-echo "process 3 terminates"
+# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_1=$!
+# wait $pid_1
+# echo "process 1 terminates"
+# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_2=$!
+# wait $pid_2
+# echo "process 2 terminates"
+# $BENCHMARK $ACCESS_PATTERN $MEMORY & pid_3=$!
+# wait $pid_3
+# echo "process 3 terminates"
+
+for i in {1..$NUM_RUNS}
+do
+	$BENCHMARK $ACCESS_PATTERN $MEMORY & pid=$!
+	wait $pid
+	echo "process $i terminates"
+done
