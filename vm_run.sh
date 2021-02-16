875b511077f6f11fa786571e408543cf8f7d3ce7
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-16 17:48:07 +0000

    Update vm_run.sh

--- vm_run.sh
+++ vm_run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -x 
 
-./triple_run.sh ./time_microbenchmark sequential 7
-./triple_run.sh ./time_microbenchmark random 7
-./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark sequential 7
+if [ -z "$1" ]
+	then
+	echo "No argument supplied"
+	exit 
+fi
+
+MEMORY=$1
+
+./triple_run.sh ./time_microbenchmark sequential $MEMORY
+./triple_run.sh ./time_microbenchmark random $MEMORY
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark sequential $MEMORY
 sleep 1
-./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random 7
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random $MEMORY:wq
