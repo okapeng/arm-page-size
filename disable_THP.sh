82b288a0eb0ae90adc20198ef40c1a53a640639c
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-02-17 11:52:34 +0000

    Create disable_THP.sh

---
+++ disable_THP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x 
+
+sh -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
+sh -c "echo never > /sys/kernel/mm/transparent_hugepage/defrag"
+cat /sys/kernel/mm/transparent_hugepage/enabled
