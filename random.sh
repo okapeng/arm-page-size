aa55351dc7ce0fb9a0a6905a530e22684644f56a
okapeng <okapengwyq@gmail.com>
2021-02-19 11:12:57 +0000

    add seq and rand

---
+++ random.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x 
+
+if [ -z "$1" ]
+	then
+	echo "No argument supplied"
+	exit 
+fi
+
+MEMORY=$1
+
+./triple_run.sh ./time_microbenchmark random $MEMORY
+sleep 1
+./multi-microbench.sh ./time_microbenchmark ./time_microbenchmark random $MEMORY
