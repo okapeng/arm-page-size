0178bcc22694f5505bde50fe154e9755d231d27d
root <root@ms0313.utah.cloudlab.us>
2021-02-20 15:30:23 -0700

    add cat meminfo

--- disable_THP.sh
+++ disable_THP.sh
@@ -3,3 +3,4 @@
 sh -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled"
 sh -c "echo never > /sys/kernel/mm/transparent_hugepage/defrag"
 cat /sys/kernel/mm/transparent_hugepage/enabled
+cat /proc/meminfo |grep Anon
