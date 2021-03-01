ea4912f0d95735a30e0f5bd71de13cca76da2c31
root <root@ms0313.utah.cloudlab.us>
2021-02-28 20:57:44 -0700

    add perf cache counter

--- triple_run.sh
+++ triple_run.sh
@@ -10,7 +10,7 @@ BENCHMARK=$1 # specify the benchmark to run
 ACCESS_PATTERN=$2 # specify the access pattern, random or sequential
 MEMORY=$3 # specify memory size for each process in GB
 
-for i in {1..10}
+for i in {1..5}
 do
 	$BENCHMARK $ACCESS_PATTERN $MEMORY & pid=$!
 	wait $pid
