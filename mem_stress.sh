3c24a4d2ee968ef78bdc61fd3488c2b353b26bbd
root <root@ms0313.utah.cloudlab.us>
2021-03-01 03:28:19 -0700

    add mem stress

---
+++ mem_stress.sh
@@ -0,0 +1,6 @@
+./triple_run.sh ./microbenchmark random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
+./multi-microbench.sh ./microbenchmark ./microbenchmark_manual random 10
