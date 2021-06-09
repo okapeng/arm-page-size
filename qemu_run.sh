a7f6c555ef084a8c2819771a29c642f752550c32
Ivy Wang <okapengwyq@gmail.com>
2021-06-09 07:58:34 +0000

    specify qumu_run output dir

--- qemu_run.sh
+++ qemu_run.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+        then
+        echo "Please enter the page size of the system"
+        exit
+fi
+
+PAGESIZE=$1
 MEMORY=10
+OUTPUT=/mnt/output/$PAGESIZE
+
+mkdir -p $OUTPUT
 echo "Start characterizing sequential microbenchmark"
-./triple_run.sh ./time_microbenchmark sequential $MEMORY >> /mnt/4kb_4kb_seq.out
+./triple_run.sh ./time_microbenchmark sequential $MEMORY >> $OUTPUT/seq_$PAGESIZE.txt
 sleep 2
 echo "Start characterizing random microbenchmark"
-./triple_run.sh ./time_microbenchmark random $MEMORY >> /mnt/4kb_4kb_rand.out
+./triple_run.sh ./time_microbenchmark random $MEMORY >> $OUTPUT/rand_$PAGESIZE.txt
--- seq_rand.sh
+++
@@ -1,2 +0,0 @@
-./sequential.sh 10 >> /mnt/4kb_4kb_seq.out
-./random.sh 10 >> /mnt/4kb_4kb_rand.out
