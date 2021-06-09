1787ef07c940091900320dc02d96a7d5b6deed76
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-06-09 17:57:47 +0100

    Update qemu_run.sh

--- qemu_run.sh
+++ qemu_run.sh
@@ -6,15 +6,15 @@ if [ -z "$1" ]
         exit
 fi
 
-if [ -z $2 ]
+if [ -z "$2" ]
         then
-        echo "THP is enabled"
-        ./enable_THP.sh
-        THP="_THP"
-        else
         echo "THP is disabled"
         ./disable_THP.sh
         THP=""
+        else
+        echo "THP is enabled"
+        ./enable_THP.sh
+        THP="_THP"
 fi
 
 PAGESIZE=$1
