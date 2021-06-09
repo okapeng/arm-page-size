6e969021302926bb8f3d6c1c357698f2c82dd6ed
Ivy Wang <35765664+okapeng@users.noreply.github.com>
2021-06-09 11:54:36 +0100

    Update qemu_run.sh

--- qemu_run.sh
+++ qemu_run.sh
@@ -6,16 +6,26 @@ if [ -z "$1" ]
         exit
 fi
 
+if [ -z "$2" ]
+        then
+        echo "THP is enabled"
+        ./enable_THP.sh
+        THP="_THP"
+        else
+        echo "THP is disabled"
+        ./disable_THP.sh
+        THP=""
+fi
+
 PAGESIZE=$1
 MEMORY=10
-OUTPUT=/mnt/output/$PAGESIZE
+OUTPUT=/mnt/output/${PAGESIZE}KB
 
-./disable_THP.sh 
 mkdir -p $OUTPUT
 echo "Start characterizing sequential microbenchmark"
-./triple_run.sh ./time_microbenchmark sequential $MEMORY >> $OUTPUT/seq_$PAGESIZE.txt
+./triple_run.sh ./time_microbenchmark sequential $MEMORY >> $OUTPUT/seq${THP}_$PAGESIZE.txt
 sleep 2
 echo "Start characterizing random microbenchmark"
-./triple_run.sh ./time_microbenchmark random $MEMORY >> $OUTPUT/rand_$PAGESIZE.txt
+./triple_run.sh ./time_microbenchmark random $MEMORY >> $OUTPUT/rand${THP}_$PAGESIZE.txt
 
 
